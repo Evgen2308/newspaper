30016c3640a62e7bcec13f3cd4217dd984bd775e
Evgen2308 <107253440+Evgen2308@users.noreply.github.com>
2023-03-10 15:57:30 +0200

    3-rd edition

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -17,12 +17,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-2^x!@a!nedtxkr2hb8uz00hpzk%1lnl*lb@fp6khft#!vs)$1i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from news import views
+
 
 urlpatterns = [
-    path('home/', views.home),
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('news/', include('news.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('', include('accounts.urls')),
 ]
